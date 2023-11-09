e6498bf99d7c39167e05da927c451bf4cd760ec3
Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>
2023-11-09 18:46:30 +0100

    Concurrently search for patterns
    Execution times were a bit high and concurrently searching
    for the search word reduces the figures; and makes this run
    a bit faster.
    
    Signed-off-by: Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>

--- search.go
+++ search.go
@@ -1,9 +1,10 @@
-package ggrep
+package main
 
 import (
 	"os"
 	"log"
 	"fmt"
+	"sync"
 	"bufio"
 	"regexp"
 	"strings"
@@ -14,6 +15,7 @@ var (
                 "Reset" : "\033[0m",
                 "Red"   : "\033[1;31m",
         }
+	match = make(chan string, 1024)
 )
 
 func FindStdIn(pattern string) {
@@ -48,21 +50,49 @@ func Find(f *os.File, pattern string) {
 }
 
 func find(file *os.File, pattern string) (string, error) {
+	var matches []string
 	scanner := bufio.NewScanner(file)
-	searchResults, err := searchLoop(scanner, pattern)
+	var wg sync.WaitGroup
 
-	return searchResults, err
+	for scanner.Scan() {
+		wg.Add(1)
+		go searchLoop(scanner.Text(), pattern, &wg)
+	}
+	wg.Wait()
+        close(match)
+
+        for v := range match {
+		matches = append(matches, v)
+		matches = append(matches, "\n")
+	}
+
+	m := strings.Join(matches, "")
+	return m, nil
 }
 
 func findStdin(pattern string) (string, error) {
+	var matches []string
 	scanner := bufio.NewScanner(os.Stdin)
-	searchResults, err := searchLoop(scanner, pattern)
+	var wg sync.WaitGroup
 
-	return searchResults, err
+	for scanner.Scan() {
+		wg.Add(1)
+		go searchLoop(scanner.Text(), pattern, &wg)
+	}
+	wg.Wait()
+        close(match)
+
+        for v := range match {
+		matches = append(matches, v)
+		matches = append(matches, "\n")
+	}
+
+	m := strings.Join(matches, "")
+	return m, nil
 }
 
-func searchLoop(scanner *bufio.Scanner, pattern string) (string, error) {
-	var matches []string
+func searchLoop(searchPattern, pattern string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	regex, err := regexp.Compile(pattern)
 
 	if err != nil {
@@ -70,19 +100,11 @@ func searchLoop(scanner *bufio.Scanner, pattern string) (string, error) {
 	}
 
 
-	for scanner.Scan() {
-		oldText := scanner.Text()
-		if regex.MatchString(oldText) {
-			newWord := []string{Colors["Red"], regex.FindString(pattern), Colors["Reset"]}
-			replacement := strings.Join(newWord, "")
-			newText := regex.ReplaceAllString(oldText, replacement)
-			matches = append(matches, newText)
-			matches = append(matches, "\n")
-		} else {
-			continue
-		}
+	oldText := searchPattern
+	if regex.MatchString(oldText) {
+		newWord := []string{Colors["Red"], regex.FindString(pattern), Colors["Reset"]}
+		replacement := strings.Join(newWord, "")
+		newText := regex.ReplaceAllString(oldText, replacement)
+		match <- newText
 	}
-
-	m := strings.Join(matches, "")
-	return m, nil
 }
