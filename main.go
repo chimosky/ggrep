f7a42731ede6464d567fd089408c9f5433da6ce7
Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>
2023-11-09 18:48:52 +0100

    Add binary
    rename ggrep.go to main.go
    
    Signed-off-by: Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+)
+
+const usage = "usage: ggrep <pattern> <filname>..."
+
+func main() {
+
+	if len(os.Args) == 1 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	pattern := os.Args[1]
+	fileNames := os.Args[2:]
+
+	if len(fileNames) == 0 {
+		FindStdIn(pattern)
+	}
+
+	for _, f := range fileNames {
+		file, err := os.Open(f)
+                defer file.Close()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		Find(file, pattern)
+	}
+}
